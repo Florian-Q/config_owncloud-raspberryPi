07f71ebad448426109bf30dcdcd3e0aef06adde9
Unknown <florian-q@protonmail.com>
2018-02-15 17:08:53 +0100

    Veracrypt
    lance le déchiffrement

---
+++ veracrypt.sh
@@ -0,0 +1,11 @@
+# install veracrypt if not exist
+if ! [ -x "$(command -v veracrypt)" ]; then
+	sudo apt-get install -y libfuse-dev makeself libwxbase3.0-0
+	cd /tmp/
+	wget https://launchpad.net/veracrypt/trunk/1.21/+download/veracrypt-1.21-raspbian-setup.tar.bz2
+	tar -vxjf ./veracrypt-1.21-raspbian-setup.tar.bz2
+	sudo chmod +x veracrypt-1.21-setup-*
+	./veracrypt-1.21-setup-console-armv7
+	sudo -u www-data mkdir -p /var/www/owncloud/data
+fi
+veracrypt -k "" --pim=0 -m=nokernelcrypto --protect-hidden=no /dev/sda1 /var/www/owncloud/data
